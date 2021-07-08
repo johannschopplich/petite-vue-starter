ede44bf1309b198224084ffe59060cb9c317c560
Johann Schopplich <mail@johannschopplich.com>
2021-07-08 17:25:37 +0200

    feat: @mounted event listener example

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import "./style.css";
-// @ts-ignore: No types available yet
+// @ts-expect-error: No types available yet
 import { createApp } from "petite-vue";
 
 createApp({
   // Exposed to all expressions
   count: 0,
+
   // Getters
   get plusOne() {
     return this.count + 1;
   },
+
   // Methods
   increment() {
     this.count++;
+  },
+
+  mounted() {
+    console.log("I'm mounted!");
   },
 }).mount();
