17435c7071f3c934027817f240e8eafd618a524b
Johann Schopplich <mail@johannschopplich.com>
2021-07-08 18:14:28 +0200

    chore: tailwind jit mode doesn't need enabled key

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
-const colors = require("tailwindcss/colors");
+const { sky, cyan } = require("tailwindcss/colors");
 
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
   mode: "jit",
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    content: ["./index.html", "./src/**/*.ts"],
-  },
+  purge: ["./index.html", "./src/**/*.ts"],
   theme: {
     extend: {
-      colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
-      },
+      colors: { sky, cyan },
     },
   },
-  variants: {},
-  plugins: [],
 };
