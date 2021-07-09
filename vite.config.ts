899bc17c51a81524ba90ad4d8972ad159815f195
Johann Schopplich <mail@johannschopplich.com>
2021-07-09 15:00:47 +0200

    chore: use mode for Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { ConfigEnv, UserConfigExport } from "vite";
 import { minifyHtml } from "vite-plugin-html";
 
-export default ({ command }: ConfigEnv): UserConfigExport => ({
-  plugins: [minifyHtml(command === "build")],
+export default ({ mode }: ConfigEnv): UserConfigExport => ({
+  plugins: [minifyHtml(mode === "production")],
 });
